b7ee3f222ece93136a612eef1fcab44618081fd3
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2021-01-28 10:14:37 -0800

    Added mailjet API and connection to Cloud Storage

--- Event_Scrape.py
+++ Event_Scrape.py
@@ -1,26 +1,28 @@
 #MODULES
 from bs4 import BeautifulSoup
 import requests
+from google.cloud import storage
+from google.cloud.storage import Blob
 
-# WEBSCRAPE SETUP
+#CONSTANTS & VARIABLES
 html_target = 'https://www.greaterseattleonthecheap.com/free-and-cheap-things-to-do-in-seattle-this-weekend/'
-source = requests.get(html_target).text
-soup = BeautifulSoup(source, 'lxml')
-
-#VARIABLES
-all_events_txt = open('test.txt', 'w+')
+TXT_FILE = 'event.txt'
+storage_client = storage.Client.from_service_account_json("python-test2-key.json")
+BUCKET_NAME = 'python-test2-bucket'
+SRC_FILE = TXT_FILE
+DEST_BLOB_NAME = TXT_FILE
 event_set = set()
 event_links = set()
 
-new_month = soup.find("h3").text
-all_events_txt.truncate(0)
+# WEBSCRAPE SETUP
+all_events_txt = open(TXT_FILE, 'w+')
+source = requests.get(html_target).text
+soup = BeautifulSoup(source, 'lxml')
 
-#READ IN PREVIOUS EVENTS
-for line in all_events_txt:
-    if line.find("http") != -1:
-        event_links.add(line.strip())
-    elif line != "":
-        event_set.add(line.strip())
+
+#ADD DATE
+new_weekend = soup.find("h3").text
+all_events_txt.write('Weekend of '+ new_weekend + ','+'\n')
 
 # #INPUT IN EVENTS
 for event in soup.find_all(class_ = "row event featured"):
@@ -28,12 +30,23 @@ for event in soup.find_all(class_ = "row event featured"):
     if not event.text in event_set:
         event_set.add(event.text)
         event_links.add(child["href"])
-        all_events_txt.write (event.h3.text)
-        all_events_txt.write('\n')
-        all_events_txt.write (child["href"])
-        all_events_txt.write('\n')
-        all_events_txt.write('\n')
+        all_events_txt.write (event.h3.text + ',' + '\n')
+        all_events_txt.write (child["href"] + '\n'*2)
+
 
 all_events_txt.close()
-# print(event_set)
 print('******WRITE COMPLETE******')
+
+#UPLOAD TO STORAGE BUCKET
+bucket = storage_client.get_bucket(BUCKET_NAME)
+
+blob = bucket.blob(SRC_FILE)
+blob.upload_from_filename(DEST_BLOB_NAME)
+
+print(
+    "File {} uploaded to {}.".format(
+        SRC_FILE, DEST_BLOB_NAME
+    )
+)
+
+
--- test.py
+++ test.py
@@ -22,28 +22,38 @@
 #     )
 # )
 
-# TEST - send email function
-# from mailjet_rest import Client
-# api_key = 'f0b62ec8d9479beeed284b8e2f97c38d'
-# api_secret = '7f57fd3681038880dd2ffa8df220c1e8'
-# mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-# data = {
-#   'Messages': [
-# 				{
-# 						"From": {
-# 								"Email": "UTP56479user@gmail.com",
-# 								"Name": "Me"
-# 						},
-# 						"To": [
-# 								{
-# 										"Email": "uyen.a.tran@gmail.com",
-# 										"Name": "You"
-# 								}
-# 						],
-# 						"Subject": "Python-test2",
-# 						"TextPart": "New events!",
-# 						"HTMLPart": "<h3> New events</h3>"
-# 				}
-# 		]
-# }
-# result = mailjet.send.create(data=data)
+
+from mailjet_rest import Client
+from google.cloud import storage
+import os
+
+storage_client = storage.Client.from_service_account_json("python-test2-key.json")
+BUCKET_NAME = 'python-test2-bucket'
+bucket = storage_client.get_bucket(BUCKET_NAME)
+filename = list(bucket.list_blobs(prefix=''))
+bucket_data = bucket.blob("event.txt").download_as_string().decode('cp1252')
+
+api_key = os.environ['MAILJETKEY']
+api_secret = os.environ['MAILJETSECRET']
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+data = {
+  'Messages': [
+				{
+						"From": {
+								"Email": "UTP56479user@gmail.com",
+								"Name": "Me"
+						},
+						"To": [
+								{
+										"Email": "uyen.a.tran@gmail.com",
+										"Name": "You"
+								}
+						],
+						"Subject": "Desktop Events Test",
+						"TextPart": "",
+						"HTMLPart": f"<h3>Greetings! Things you should look into this weekend: </h3> <div style='white-space: pre-wrap'>{bucket_data}</div>"
+				}
+		]
+}
+result = mailjet.send.create(data=data)
+print('email sent.')
