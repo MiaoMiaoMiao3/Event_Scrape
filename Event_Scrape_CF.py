725a14c1d68302f14034aa6e350f00793ac254e1
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2021-01-29 13:13:00 -0800

    Created readme file, general code reformatting

--- Event_Scrape_CF.py
+++ Event_Scrape_CF.py
@@ -1,28 +1,3 @@
-# TEST - Uploading file to bucket
-# from google.cloud import storage
-# from google.cloud.storage import Blob
-
-# #Create a client object
-# storage_client = storage.Client.from_service_account_json("python-test2-key.json")
-
-# #CONSTANTS
-# BUCKET_NAME = 'python-test2-bucket'
-# source_file_name = "test2.txt"
-# destination_blob_name = "test2.txt"
-
-
-# bucket = storage_client.get_bucket(BUCKET_NAME)
-
-# blob = bucket.blob(source_file_name)
-# blob.upload_from_filename(destination_blob_name)
-
-# print(
-#     "File {} uploaded to {}.".format(
-#         source_file_name, destination_blob_name
-#     )
-# )
-
-
 from mailjet_rest import Client
 from google.cloud import storage
 import os
@@ -40,12 +15,12 @@ data = {
   'Messages': [
 				{
 						"From": {
-								"Email": "UTP56479user@gmail.com",
+								"Email": os.environ["SENDER"],
 								"Name": "Me"
 						},
 						"To": [
 								{
-										"Email": "uyen.a.tran@gmail.com",
+										"Email": os.environ["RECEIVER"],
 										"Name": "You"
 								}
 						],
---
+++ README.md
@@ -0,0 +1,17 @@
+# Scraping the Web and Sending Emails With Google Cloud
+
+## Overview
+Like most people today, I find it hard to discover things outside of the house that I can actually do with all the social distancing restrictions in place <br/>
+(a girl can only watch so much Netflix). I decided to write a program to combat the cabin fever by scraping the web for events around my home town and sending <br/>
+me a email weekly with events found. It seemed like a worthy cause. 😊 <br/>
+
+## Requirements
+To execute this program you will need:
+- A Google Cloud account
+- An email delivery service account (I used Mailjet in this case)
+
+
+## Architecture Diagram
+![architecture diagram](https://drive.google.com/uc?export=view&id=1nkhoop7bb8wDr8_b8Q_gng5VKIBkSvkh)
+https://drive.google.com/uc?export=view&id=1nkhoop7bb8wDr8_b8Q_gng5VKIBkSvkh
+ 
